9e911a9b4ea6fb8df953b9ec1979429576563a85
Tushyap <88049138+Tushyap@users.noreply.github.com>
2022-09-01 22:27:21 +0530

    Add files via upload

---
+++ airportscheduleFlight/src/main/java/com/airportscheduleFlight/AirportscheduleFlightApplication.java
@@ -0,0 +1,13 @@
+package com.airportscheduleFlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportscheduleFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportscheduleFlightApplication.class, args);
+	}
+
+}
---
+++ airportscheduleFlight/src/test/java/com/airportscheduleFlight/AirportscheduleFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airportscheduleFlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportscheduleFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
