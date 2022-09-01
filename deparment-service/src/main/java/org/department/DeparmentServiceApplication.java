eed557efff2aa933c4068832c670d17fa0b85cc6
Tushyap <88049138+Tushyap@users.noreply.github.com>
2022-09-01 22:10:03 +0530

    Add files via upload

---
+++ deparment-service/src/main/java/org/department/DeparmentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeparmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeparmentServiceApplication.class, args);
+	}
+
+}
